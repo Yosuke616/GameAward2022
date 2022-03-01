c4888597045f8a112247cd8a48946c232c2b314d
Yosuke_616_GameAward <y.s2000616@docomo.ne.jp>
2022-03-01 19:17:04 +0900

    ページを変えるためのシーンと仮のテクスチャを用意しました。

---
+++ Assets/Scripts/TurnPaperScript.cs
@@ -0,0 +1,24 @@
+/*
+ 2022/3/1 u…—z—S 
+ †‚ğ‚ß‚­‚é‚½‚ß‚ÌƒXƒNƒŠƒvƒg
+ 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnPaperScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

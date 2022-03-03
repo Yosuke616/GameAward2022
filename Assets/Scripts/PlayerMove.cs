3b3be8de6d0c8475157075614d712eb647db7a75
Cena_Aiba <xiangyuxingna@gmail.com>
2022-03-03 21:56:54 +0900

    コントローラー操作の実装&プレイヤームーブ

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,18 @@
 /*
  2022/2/28 志水陽祐
+
+ 2022/3/02 相羽星那・・・左右の自動移、下矢印で止まる
+
+
  プレイヤーの操作と地面の設定 
  */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+
 
 public class PlayerMove : MonoBehaviour
 {
@@ -16,69 +23,105 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] public bool inJumping = false;
 
     //rigidbodyオブジェクト格納用変数
-    Rigidbody rb;
+    //Rigidbody rb;
 
-    //移動速度の定義(数値を変えてスピードを変える) 
-    float Speed = 6.0f;
+    public enum EMoveCharacter
+    {
+        STOP_MOVE = 0,
+        RIGHT_MOVE,
+        LEFT_MOVE,
 
-    //ダッシュ速度の定義
-    float SprintSpeed = 9.0f;
 
-    //移動の係数格納用変数の定義
-    float fv;
+        MAX_MOVE
+    }
+
+
+    //速度
+    public float speed = 5.0f;
+    public float jump = 5.0f;
+
+    private Rigidbody rb;
+
+    public EMoveCharacter eCharaMove;
+    private bool isjump;
 
     void Start()
     {
-        //プレイヤーのrigidbodyを持ってくる
-        rb = this.GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb = GetComponent<Rigidbody>();
+        eCharaMove = EMoveCharacter.STOP_MOVE;
+        isjump = false;
+        //rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Shift+左右キーでダッシュ、左右で歩き
-        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftShift)) {          //右ダッシュ
-            fv = Time.deltaTime * SprintSpeed;
-        }else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.LeftShift)){      //左ダッシュ
-            fv = -Time.deltaTime * SprintSpeed;
-        }else if (Input.GetKey(KeyCode.RightArrow)){        //右歩き
-            fv = Time.deltaTime * Speed;
-        }else if (Input.GetKey(KeyCode.LeftArrow)){         //左歩き
-            fv = -Time.deltaTime * Speed;
-        }else {
-            fv = 0;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetAxis("Vertical") > 0 && !isjump)
+        {
+            rb.velocity = Vector3.up * jump;
+            isjump = true;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetAxis("Horizontal") < 0)
+        {
+            eCharaMove = EMoveCharacter.RIGHT_MOVE;
+            //transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") > 0)
+        {
+            eCharaMove = EMoveCharacter.LEFT_MOVE;
+            //transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("Vertical") < 0)
+        {
+            eCharaMove = EMoveCharacter.STOP_MOVE;
+        }
+
+        switch (eCharaMove)
+        {
+            case EMoveCharacter.STOP_MOVE:
+                break;
+            case EMoveCharacter.RIGHT_MOVE:
+                transform.position += transform.right * speed * Time.deltaTime;
+                break;
+            case EMoveCharacter.LEFT_MOVE:
+                transform.position -= transform.right * speed * Time.deltaTime;
+                break;
+
         }
 
         //移動の実行
         //if (!inJumping) {   //空中での移動を禁止
-            transform.position += transform.forward * fv;
+        //transform.position += transform.forward * fv;
         //}
 
         //スペースでジャンプする
-        if (onGround) {
-            if (Input.GetKey(KeyCode.Space)) {
-                //ジャンプさせるために上方向の力を加える
-                rb.AddForce(transform.up * 8, ForceMode.Impulse);
+        //if (onGround)
+        //{
+        //    if (Input.GetKey(KeyCode.Space))
+        //    {
+        //        //ジャンプさせるために上方向の力を加える
+        //        rb.AddForce(transform.up * 8, ForceMode.Impulse);
 
-                //ジャンプ中の地面の接触判定はfalseに変える
-                onGround = false;
-                inJumping = true;
+        //        //ジャンプ中の地面の接触判定はfalseに変える
+        //        onGround = false;
+        //        inJumping = true;
 
-                //左右キー押しながらジャンプしたときは左右方向に力を加える
-                if (Input.GetKey(KeyCode.RightArrow)) {
-                    rb.AddForce(transform.forward * 6.0f, ForceMode.Impulse);
-                } else if (Input.GetKey(KeyCode.LeftArrow)) {
-                    rb.AddForce(transform.forward * -3.0f, ForceMode.Impulse);
-                }
+        //        //左右キー押しながらジャンプしたときは左右方向に力を加える
+        //        if (Input.GetKey(KeyCode.RightArrow)) {
+        //            rb.AddForce(transform.forward * 6.0f, ForceMode.Impulse);
+        //        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+        //            rb.AddForce(transform.forward * -3.0f, ForceMode.Impulse);
+        //        }
 
-            }
-        }
+        //    }
+        //}
     }
 
     //地面に接触したらonGroundをtrue、inJumpingをfalseにする
-    void OnCollisionEnter(Collision col) {
-        if (col.gameObject.tag == "Ground") {
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
             onGround = true;
             inJumping = false;
         }

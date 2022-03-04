fa9ef43105c816b211dc291f8593f8df9610116b
Sakai1356 <ce3pbftyv4@privaterelay.appleid.com>
2022-03-04 16:55:04 +0900

    紙からオブジェクト生成、オブジェクトが正しい大きさに変換されず絶賛発狂修正中

--- Assets/Scripts/Game/GenerateObjectFromPaper.cs
+++ Assets/Scripts/Game/GenerateObjectFromPaper.cs
@@ -1,13 +1,19 @@
+/* UI座標を取得、ワールド座標へ変換
+ * 参考元：https://alien-program.hatenablog.com/entry/2017/08/06/164258
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateObjectFromPaper : MonoBehaviour
 {
+	private Camera mainCamera;
 	private List<GameObject> ChildObject;
 
 	void Start()
 	{
+		mainCamera = Camera.main;
 		ChildObject = new List<GameObject>();
 		Debug.Log($"<ChildObject>");
 
@@ -20,35 +26,46 @@ public class GenerateObjectFromPaper : MonoBehaviour
 			ChildObject.Add(cTransform.gameObject);
 			// }
 		}
-		for (int i = 0; i < ChildObject.Count; i++)
-		{
-			Debug.Log($"List[{i}]:{ChildObject[i].name}");
-		}
+		//for (int i = 0; i < ChildObject.Count; i++)
+		//{
+		//	Debug.Log($"List[{i}]:{ChildObject[i].name}");
+		//}
 
 		foreach (GameObject PaperObject in ChildObject)
 		{
 			GameObject BookObject;
 
 			bool isPrefab = false;
-			PrimitiveType primitiveType = PrimitiveType.Cube;
-			Vector3 pos = new Vector3(0.123456789f, 0.987654321f, 0);
-			Quaternion rot = new Quaternion(0.123456789f, 0.987654321f, 0, 0);
+			PrimitiveType primitiveType = (PrimitiveType)(-1);
+			RectTransform rect = PaperObject.GetComponent<RectTransform>();
+			// 座標
+			Vector3 pos = rect.position;
+			// 角度
+			Quaternion rot = new Quaternion(0.123456789f, 0.123456789f, 0, 0);
+			// 縦横
+			Vector2 SizeDelta = PaperObject.GetComponent<RectTransform>().sizeDelta;
+			Vector3 scl = new Vector3(SizeDelta.x, SizeDelta.y, 0.0f);
+			Vector3 sclx = Vector3.zero;
+			Vector3 scly = Vector3.zero;
+			sclx.x = SizeDelta.x;
+			scly.y = SizeDelta.y;
+			Debug.Log($"x({sclx.x},{sclx.y},{sclx.z}),y({scly.x},{scly.y},{scly.z})");
+			sclx = this.transform.TransformPoint(sclx);
+			scly = this.transform.TransformPoint(scly);
+			Debug.Log($"x({sclx.x},{sclx.y},{sclx.z}),y({scly.x},{scly.y},{scly.z})");
+			scl = new Vector3(sclx.x, scly.y, 0.0f);
 
-			switch(PaperObject.tag)
+			switch (PaperObject.tag)
 			{
 				case "Player":
 					isPrefab = false;
 					primitiveType = PrimitiveType.Cube;
-					pos = PaperObject.transform.position;
 					rot = Quaternion.identity;
 					break;
 				case "Ground":
 					isPrefab = false;
 					primitiveType = PrimitiveType.Plane;
-					pos = PaperObject.transform.position;
-					Debug.Log($":{PaperObject.GetComponent<RectTransform>().sizeDelta.y / 2}");
-					Debug.Log($":{pos.y}");
-					pos.y += PaperObject.GetComponent<RectTransform>().sizeDelta.y / 2;
+					//pos.y += rect.sizeDelta.y / 2;
 					rot = Quaternion.identity;
 					break;
 				default:
@@ -62,9 +79,15 @@ public class GenerateObjectFromPaper : MonoBehaviour
 			}
 			else
 			{
+				//UI座標からスクリーン座標に変換
+				Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(mainCamera, pos);
+				//スクリーン座標→ワールド座標に変換
+				RectTransformUtility.ScreenPointToWorldPointInRectangle(rect, screenPos, mainCamera, out pos);
+
 				BookObject = GameObject.CreatePrimitive(primitiveType);
 				BookObject.transform.position = pos;
 				BookObject.transform.rotation = rot;
+				BookObject.transform.localScale = scl;
 			}
 			BookObject.name = PaperObject.name;
 			BookObject.tag = PaperObject.tag;

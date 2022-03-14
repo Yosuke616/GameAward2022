60915eeb088fcd7254b6c98c7dff75e313cdf60f
Es0082 <kensatou0802@icloud.com>
2022-03-14 12:47:47 +0900

    ポーズシーンの追加、キーボードのｐかコントローラーのStartでポーズ画面の表示 再開、やり直しまで作成

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;  // シーン遷移用
+using UnityEngine.UI;               // UI用
+
+public class Pause : MonoBehaviour
+{
+    // ポーズした時に表示するUI
+    [SerializeField]
+    private GameObject PauseUI;
+
+    // ポーズ画面の最初の選択されているボタン
+    public Button FirstSelectButton;
+
+    void Start()
+    {
+        // 最初の選択
+        FirstSelectButton.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キーボードの p かコントローラーのStartで入力
+        if (Input.GetKeyDown("p") || Input.GetKeyDown("joystick button 7"))
+        {
+            //　ポーズUIのアクティブ、非アクティブを切り替え
+            PauseUI.SetActive(!PauseUI.activeSelf);
+
+            //　ポーズUIが表示されてる時は停止
+            if (PauseUI.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    // リトライ
+    public void OnRetry()
+    {
+        // 同一シーンを読込
+        SceneManager.LoadScene("Pause");
+        Time.timeScale = 1f;
+    }
+
+
+    // 再開
+    public void OnResume()
+    {
+        //　ポーズUIのアクティブ、非アクティブを切り替え
+        PauseUI.SetActive(!PauseUI.activeSelf);
+        Time.timeScale = 1f;
+    }
+}

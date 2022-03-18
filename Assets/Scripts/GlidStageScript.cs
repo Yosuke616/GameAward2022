2f402be7f126756e8e5408b66dc483817d0e5fe0
Yosuke_616_GameAward <y.s2000616@docomo.ne.jp>
2022-03-18 14:39:36 +0900

    新しいシーンとスクリプトの追加

---
+++ Assets/Scripts/GlidStageScript.cs
@@ -0,0 +1,23 @@
+/*
+ 2022/3/18 ShimizuYosuke 
+ 画面いっぱいに当たり判定のある透明四角いオブジェクトを生成する(横400、縦300)
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlidStageScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

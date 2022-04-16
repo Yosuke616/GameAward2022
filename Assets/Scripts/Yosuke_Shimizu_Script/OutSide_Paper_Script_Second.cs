ce08477251f4bcc81c6777e676248ee688a6eb37
Yosuke_616_GameAward <y.s2000616@docomo.ne.jp>
2022-04-16 23:56:04 +0900

    妖精途中経過

--- Assets/Scripts/Yosuke_Shimizu_Script/OutSide_Paper_Script_Second.cs
+++ Assets/Scripts/Yosuke_Shimizu_Script/OutSide_Paper_Script_Second.cs
@@ -50,7 +50,7 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
         g_bFirst_Load = false;
 
         //紙の外周用の変数を初期化する
-        OutPaper_Pos = new Vector3(0.0f,0.0f,0.0f);
+        OutPaper_Pos = new Vector3(0.0f, 0.0f, 0.0f);
 
         //最初のフラグはfalseになっている(trueで一回目は終了)
         First_Flg = false;
@@ -61,7 +61,8 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
     void Update()
     {
         //一回だけ読み込めるようにするやつ
-        if (!g_bFirst_Load) {
+        if (!g_bFirst_Load)
+        {
             //アウトラインの情報を持つオブジェクトを探してきてリストの情報を貰う
             //出来なかったらこっちでやろう
             //紙のオブジェクトを見つけてくる
@@ -82,10 +83,10 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
         Vector2 Mouse_Pos = Input.mousePosition;
 
         //画面の半分の大きさをマイナスすることにより整合性を図るぜ
-        Mouse_Pos.x -= Screen.width/2;
-        Mouse_Pos.y -= Screen.height/2;
+        Mouse_Pos.x -= Screen.width / 2;
+        Mouse_Pos.y -= Screen.height / 2;
 
-        
+
 
         //破るためのスクリプトを用意する
         GameObject Cur = GameObject.Find("Cursor");
@@ -96,33 +97,36 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
             CursorBreak();
             First_Flg = false;
         }
-        else {
+        else
+        {
             Old_Click_Pos = Input.mousePosition;
             Old_Click_Pos.z = 10.0f;
 
 
             var Pos2 = Camera.main.ScreenToWorldPoint(Old_Click_Pos);
 
-                //一回クリックされたらそこでカーソルは止まる
-                if ((!First_Flg))
+            //一回クリックされたらそこでカーソルは止まる
+            if ((!First_Flg))
+            {
+                //マウスと中心とそれぞれどの辺かを考える(関数を作って) 
+                Cross_Pos = CalculationVector(Mouse_Pos, Paper_Center, OutLinePaper);
+
+                //カーソルをセットした座標に移動させる
+                this.transform.position = Cross_Pos;
+
+                OutPaper_Pos = Cross_Pos;
+
+                if (Input.GetMouseButtonDown(0))
                 {
-                    //マウスと中心とそれぞれどの辺かを考える(関数を作って) 
-                    Cross_Pos = CalculationVector(Mouse_Pos, Paper_Center, OutLinePaper);
-
-                    //カーソルをセットした座標に移動させる
-                    this.transform.position = Cross_Pos;
-
-                    OutPaper_Pos = Cross_Pos;
-
-                    if (Input.GetMouseButtonDown(0)) {
-                        First_Flg = true;
-                    }
-
+                    First_Flg = true;
                 }
-                else {
-                    this.transform.position = Cross_Pos;
-                    Debug.Log(234567890);
-                }
+
+            }
+            else
+            {
+                this.transform.position = Cross_Pos;
+                Debug.Log(234567890);
+            }
 
         }
     }
@@ -130,34 +134,38 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
     //線分と線分の計算をするための関数
     //戻り値:計算して出した二次元ベクトル
     //引数  :マウスの座標と紙の中心の座標と紙の4辺の情報が入ったリスト
-    private Vector2 CalculationVector(Vector2 Mouse,Vector2 Center,List<Vector3> Square) {
+    private Vector2 CalculationVector(Vector2 Mouse, Vector2 Center, List<Vector3> Square)
+    {
         //最後に返すときのための2次元ベクトル
-        Vector2 CrossVector = new Vector2(0.0f,0.0f);
+        Vector2 CrossVector = new Vector2(0.0f, 0.0f);
 
         //マウスと中心が重なっていたら速攻で戻り値(0,0)を返す
-        if (((Mouse.x - Center.x) == 0) && ((Mouse.y - Center.y) == 0)){
+        if (((Mouse.x - Center.x) == 0) && ((Mouse.y - Center.y) == 0))
+        {
             return CrossVector;
         }
 
         //4つの辺農地どれかが交わるまで繰り返す
-        for (int i = 0;i < Square.Count;i++) {
+        for (int i = 0; i < Square.Count; i++)
+        {
             //順番に外周を使っていく
             //ここで外周の長さが出るがこれは使わない
             //この考え方事態は必要
             Vector2 outlineEdge = Square[(i + 1) % (Square.Count)] - Square[i];
 
             //振る番号を作る
-            int VegetaNum = ((i+1)%(Square.Count));
+            int VegetaNum = ((i + 1) % (Square.Count));
 
             //割れる用の数字を用意する
-            float Num = (Mouse.x-Center.x)*(Square[VegetaNum].y-Square[i].y)-(Mouse.y-Center.y)*(Square[VegetaNum].x-Square[i].x);
+            float Num = (Mouse.x - Center.x) * (Square[VegetaNum].y - Square[i].y) - (Mouse.y - Center.y) * (Square[VegetaNum].x - Square[i].x);
 
             //縦と横の計算をする
-            float X = ((Square[i].x-Center.x)*(Square[VegetaNum].y-Square[i].y)-(Square[i].y-Center.y)*(Square[VegetaNum].x-Square[i].x))/Num;
-            float Y = ((Square[i].x-Center.x)*(Mouse.y-Center.y)-(Square[i].y-Center.y)*(Mouse.x-Center.x))/Num;
+            float X = ((Square[i].x - Center.x) * (Square[VegetaNum].y - Square[i].y) - (Square[i].y - Center.y) * (Square[VegetaNum].x - Square[i].x)) / Num;
+            float Y = ((Square[i].x - Center.x) * (Mouse.y - Center.y) - (Square[i].y - Center.y) * (Mouse.x - Center.x)) / Num;
 
             //範囲外だったら次の数字に行く
-            if (X < 0.0f || X > 1.0f || Y < 0.0f || Y > 1.0f) {
+            if (X < 0.0f || X > 1.0f || Y < 0.0f || Y > 1.0f)
+            {
                 continue;
             }
 
@@ -165,7 +173,7 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
             CrossVector.x = Center.x + X * (Mouse.x - Center.x);
             CrossVector.y = Center.y + X * (Mouse.y - Center.y);
 
-            
+
             //Debug.Log(CrossVector.x);
             //Debug.Log(CrossVector.y);
 
@@ -177,12 +185,14 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
     }
 
     //妖精の場所を変更するためのゲッター
-    public Vector2 GetCursorPos() {
+    public Vector2 GetCursorPos()
+    {
         return Cross_Pos;
     }
 
     //アウトラインのリストを更新する為の関数
-    public void SetMoveLine(List<Vector3> Line,Vector3 Center) {
+    public void SetMoveLine(List<Vector3> Line, Vector3 Center)
+    {
         //リストの更新をする
         OutLinePaper = Line;
 
@@ -191,10 +201,12 @@ public class OutSide_Paper_Script_Second : MonoBehaviour
     }
 
     //破る時にカーソルをそこに移動させる
-    private void CursorBreak() {
-        Pos = new Vector3(0.0f,0.0f,0.0f);
+    private void CursorBreak()
+    {
+        Pos = new Vector3(0.0f, 0.0f, 0.0f);
         //ボタンを押した座標に持っていく
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             Old_Click_Pos = Input.mousePosition;
             Old_Click_Pos.z = 10.0f;
         }

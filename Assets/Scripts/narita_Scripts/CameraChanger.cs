3c7b9cfcbf267feea0d31b987a3723dd3eb70d79
Sakai1356 <ce3pbftyv4@privaterelay.appleid.com>
2022-04-20 11:15:49 +0900

    CameraChanger.csにてSubCameraのList化

--- Assets/Scripts/narita_Scripts/CameraChanger.cs
+++ Assets/Scripts/narita_Scripts/CameraChanger.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class CameraChanger : MonoBehaviour
 {
     public Camera MainCamera;
-    public Camera SubCamera0;
-    public Camera SubCamera1;
-    public Camera SubCamera2;
-    public Camera SubCamera3;
+	public List<Camera> SubCamera;
+    //public Camera SubCamera0;
+    //public Camera SubCamera1;
+    //public Camera SubCamera2;
+    //public Camera SubCamera3;
 
     // Start is called before the first frame update
     void Start()
     {
         MainCamera.enabled = true;
-        SubCamera0.enabled = false;
-        SubCamera1.enabled = false;
-        SubCamera2.enabled = false;
-        SubCamera3.enabled = false;
-
-
+		GameObject[] objects = GameObject.FindGameObjectsWithTag("SubCamera");
+		for (int i = 0; i < objects.Length; i++)
+		{
+			SubCamera[i] = objects[i].GetComponent<Camera>();
+			SubCamera[i].enabled = false;
+		}
     }
 
     // Update is called once per frame
@@ -29,43 +30,47 @@ public class CameraChanger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F1))
         {
             MainCamera.enabled = true;
-            SubCamera0.enabled = false;
-            SubCamera1.enabled = false;
-            SubCamera2.enabled = false;
-            SubCamera3.enabled = false;
+			for(int i = 0; i < SubCamera.Count; i++)
+			{
+				SubCamera[i].enabled = false;
+			}
         }
         else if (Input.GetKeyDown(KeyCode.F2))
         {
             MainCamera.enabled = false;
-            SubCamera0.enabled = true;
-            SubCamera1.enabled = false;
-            SubCamera2.enabled = false;
-            SubCamera3.enabled = false;
-        }
+			for (int i = 0; i < SubCamera.Count; i++)
+			{
+				SubCamera[i].enabled = false;
+			}
+			SubCamera[0].enabled = true;
+		}
         else if(Input.GetKeyDown(KeyCode.F3))
         {
-            MainCamera.enabled = false;
-            SubCamera0.enabled = false;
-            SubCamera1.enabled = true;
-            SubCamera2.enabled = false;
-            SubCamera3.enabled = false;
-        }
-        else if (Input.GetKeyDown(KeyCode.F4))
+			MainCamera.enabled = false;
+			for (int i = 0; i < SubCamera.Count; i++)
+			{
+				SubCamera[i].enabled = false;
+			}
+			SubCamera[1].enabled = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.F4))
         {
-            MainCamera.enabled = false;
-            SubCamera0.enabled = false;
-            SubCamera1.enabled = false;
-            SubCamera2.enabled = true;
-            SubCamera3.enabled = false;
-        }
-        else if (Input.GetKeyDown(KeyCode.F5))
+			MainCamera.enabled = false;
+			for (int i = 0; i < SubCamera.Count; i++)
+			{
+				SubCamera[i].enabled = false;
+			}
+			SubCamera[2].enabled = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.F5))
         {
-            MainCamera.enabled = false;
-            SubCamera0.enabled = false;
-            SubCamera1.enabled = false;
-            SubCamera2.enabled = false;
-            SubCamera3.enabled = true;
-        }
-    }
+			MainCamera.enabled = false;
+			for (int i = 0; i < SubCamera.Count; i++)
+			{
+				SubCamera[i].enabled = false;
+			}
+			SubCamera[3].enabled = true;
+		}
+	}
 
 }

b8fac15652f33333f734e92329fc088b78591e2b
Es0082 <kensatou0802@icloud.com>
2022-05-09 13:26:59 +0900

    セレクトの若干調整

--- Assets/Scripts/KenKen_Script/SaveData.cs
+++ Assets/Scripts/KenKen_Script/SaveData.cs
@@ -4,6 +4,6 @@ public class SaveData
 {
     // ƒZ[ƒu‚µ‚½‚¢‚à‚Ì
     public int Progress;
-    //public int[] Timer = new int[8];
-    //public int[] Star = new int[8];
+    public int[] Timer = new int[8];
+    public int[] Star = new int[8];
 }

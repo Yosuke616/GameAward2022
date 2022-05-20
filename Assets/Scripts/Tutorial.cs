847412ec8e31665f9a41e33aee17441fdb857cc9
Sakai1356 <ce3pbftyv4@privaterelay.appleid.com>
2022-05-20 17:31:31 +0900

    chu-toriaru進捗 99.9-20%完了

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -48,10 +48,11 @@ public class Tutorial : MonoBehaviour
 		BGobjects.Add(TutorialPanel.transform.Find("BackGround1").gameObject);
 		BGobjects.Add(TutorialPanel.transform.Find("BackGround2").gameObject);
 		BGobjects.Add(TutorialPanel.transform.Find("BackGround3").gameObject);
-		//BGobjects.Add(TutorialPanel.transform.Find("BackGround4").gameObject);
-		//BGobjects.Add(TutorialPanel.transform.Find("BackGround5").gameObject);
-		//BGobjects.Add(TutorialPanel.transform.Find("BackGround6").gameObject);
-		//BGobjects.Add(TutorialPanel.transform.Find("BackGround7").gameObject);
+		BGobjects.Add(TutorialPanel.transform.Find("BackGround4").gameObject);
+		BGobjects.Add(TutorialPanel.transform.Find("BackGround5").gameObject);
+		BGobjects.Add(TutorialPanel.transform.Find("BackGround6").gameObject);
+		BGobjects.Add(TutorialPanel.transform.Find("BackGround7").gameObject);
+		BGobjects.Add(TutorialPanel.transform.Find("BackGround8").gameObject);
 
 		txt_ComeOn = GameObject.Find("txt_ComeOn");
 		txt_CutStart = GameObject.Find("txt_CutStart");
@@ -111,14 +112,14 @@ public class Tutorial : MonoBehaviour
 					BGobjects[nCnt].SetActive(false);
 
 					// 現在の要素数がリストの要素数より小さいとき（説明中）の処理
-					if (nCnt + 1 < BGobjects.Count)
+					//if (nCnt + 1 < 4)
 					{
 						BGobjects[nCnt + 1].SetActive(true);
-						nCnt++;
 					}
 
 					// 説明中ではない時、切断操作を有効にする
-					else
+					//else
+					if (nCnt + 1 >= 4)
 					{
 						cursor.SetActive(true);
 						turnPaper.SetActive(true);
@@ -126,35 +127,57 @@ public class Tutorial : MonoBehaviour
 						Yousei1.GetComponent<Fiary_Move>().enabled = true;
 						Yousei2.GetComponent<Fiary_Script>().enabled = true;
 						Yousei2.GetComponent<Fiary_Move>().enabled = true;
-						bEndTutorial = true;
 						Debug.LogWarning($"Tuto:end[{bEndTutorial}]");
 					}
+					nCnt++;
 
 					switch (nCnt)
 					{
+						case 0:
+							break;
+
 						// 0~1
 						case 1:
+							Debug.LogWarning($"{nCnt}");
 							bStop = true;
 							turnPaper.SetActive(true);
 							BGobjects[nCnt].SetActive(false);
 							break;
 
+						case 2:
+							break;
+
 						// 2~3
 						case 3:
+							Debug.LogWarning($"{nCnt}");
 							bStop = true;
 							turnPaper.SetActive(true);
 							BGobjects[nCnt].SetActive(false);
 							break;
 
 						case 4:
-							txt_SonoChoshi.SetActive(true);
+							Debug.LogWarning($"{nCnt}");
+							txt_CutStart.SetActive(false);
 							break;
 
 						case 5:
-							txt_Koko.SetActive(true);
+							txt_SonoChoshi.SetActive(true);
 							break;
 
 						default:
+							switch (nCnt % 2)
+							{
+								case 0:
+									Debug.LogWarning($"{nCnt}");
+									txt_SonoChoshi.SetActive(true);
+									txt_Koko.SetActive(false);
+									break;
+								case 1:
+									Debug.LogWarning($"{nCnt}");
+									txt_SonoChoshi.SetActive(false);
+									txt_Koko.SetActive(true);
+									break;
+							}
 							break;
 					}
 
@@ -199,5 +222,28 @@ public class Tutorial : MonoBehaviour
 			}
 
 		}
+
+		//if (bStartTutorial == true && bEndTutorial == true)
+		//{
+		//	// 次のページへ
+		//	if (Input.GetMouseButtonDown(0))
+		//	{
+		//		txt_CutStart.SetActive(false);
+		//		nCnt++;
+		//		switch (nCnt % 2)
+		//		{
+		//			case 0:
+		//				Debug.LogWarning($"{nCnt}");
+		//				txt_SonoChoshi.SetActive(true);
+		//				txt_Koko.SetActive(false);
+		//				break;
+		//			case 1:
+		//				Debug.LogWarning($"{nCnt}");
+		//				txt_SonoChoshi.SetActive(false);
+		//				txt_Koko.SetActive(true);
+		//				break;
+		//		}
+		//	}
+		//}
 	}
 }

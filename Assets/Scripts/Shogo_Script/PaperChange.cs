0be84b8a147cf03172fef8154a6c0935f1f450cc
ShogoShimizu <sho_gogo_0104@icloud.com>
2022-05-21 21:55:42 +0900

    ウサギアニメーションからゲームに切り替わる時の白フェード完成。フェードのスピードを調整完了。

---
+++ Assets/Scripts/Shogo_Script/PaperChange.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaperChange : MonoBehaviour
+{
+    [SerializeField]
+    private Image _paperChange;
+    [SerializeField]
+    private float FADESPEED = 0.1f;
+
+    private float valueSpeed;
+
+    private bool _isFadeIn, _isFadeOut;
+
+    public Camera openingCamera;
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _paperChange.gameObject.SetActive(false);
+        _isFadeOut = false;
+        _isFadeIn = false;
+        valueSpeed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isFadeOut)
+        {
+            FadeOut();
+        }
+        else if (_isFadeIn)
+        {
+            FadeIn();
+        }
+    }
+
+    public void FadeStart()
+    {
+        _paperChange.gameObject.SetActive(true);
+
+        _isFadeOut = true;
+    }
+
+    private void FadeOut()
+    {
+        valueSpeed += FADESPEED * Time.deltaTime;
+        _paperChange.material.SetFloat("_Value", valueSpeed);
+
+        if (valueSpeed >= 1.0f)
+        {
+            valueSpeed = 1.0f;
+            _isFadeOut = false;
+            _isFadeIn = true;
+
+            openingCamera.enabled = false;
+            mainCamera.enabled = true;
+        }
+    }
+
+    private void FadeIn()
+    {
+        valueSpeed -= FADESPEED * Time.deltaTime;
+        _paperChange.material.SetFloat("_Value", valueSpeed);
+
+        if (valueSpeed <= 0.0f)
+        {
+            valueSpeed = 0.0f;
+            _paperChange.gameObject.SetActive(false);
+            _isFadeIn = false;
+        }
+    }
+}

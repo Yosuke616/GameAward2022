828d76ae40d576fec8c0bc8d560704ec9f7b7ebc
Sakai1356 <ce3pbftyv4@privaterelay.appleid.com>
2022-05-21 12:32:23 +0900

    tutorial見た目改良（まだ仮状態）

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,3 +1,4 @@
+// Tutorialシーン専用スクリプト
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
---
+++ Assets/Scripts/yanikasu_scr/ImageBlinking.cs
@@ -0,0 +1,40 @@
+// Image点滅表現スクリプト
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageBlinking : MonoBehaviour
+{
+	public float fVol;
+
+	private bool bUp;
+	private Color clr;
+    // Start is called before the first frame update
+    void Start()
+    {
+		fVol = 0.025f;
+		bUp = false;
+		clr = GetComponent<Image>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.LogWarning($"bUp:{bUp}, Alpha:{clr.a}, fVol:{fVol}");
+		if(bUp)
+		{
+			clr = new Color(clr.r, clr.g, clr.b, clr.a + fVol);
+			GetComponent<Image>().color = clr;
+			if (clr.a >= 1.0f)
+				bUp = false;
+		}
+		else
+		{
+			clr = new Color(clr.r, clr.g, clr.b, clr.a - fVol);
+			GetComponent<Image>().color = clr;
+			if (clr.a <= 0.0f)
+				bUp = true;
+		}
+    }
+}

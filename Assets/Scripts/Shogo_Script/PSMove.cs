528b6f3c8243e85f671a3ba249ed9533fbd3c414
ShogoShimizu <sho_gogo_0104@icloud.com>
2022-05-22 19:42:08 +0900

    スライドの速度に慣性を与えた。慣性の完成ですね。

--- Assets/Scripts/Shogo_Script/PSMove.cs
+++ Assets/Scripts/Shogo_Script/PSMove.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PSMove : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = 12.0f;
+    private float moveSpeed = 0.6f;
+
+    private float minusNum;
+
+    private Vector3 pos;
 
     private bool moveLeft, moveRight;
 
@@ -19,6 +23,8 @@ public class PSMove : MonoBehaviour
 
         tmp = gameObject.transform.position;
         targetPos = new Vector3(-CreateTriangle.paperSizeX * 2.0f - 2.0f, tmp.y, tmp.z);
+        pos = new Vector3(0.0f, tmp.y, tmp.z);
+        minusNum = 0.0f;
     }
 
     // Update is called once per frame
@@ -26,20 +32,61 @@ public class PSMove : MonoBehaviour
     {
         if (moveLeft)
         {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            Debug.Log("左");
+
+            pos.x += moveSpeed;
+
+            transform.Translate(Vector3.left * pos.x * Time.deltaTime);
             if (transform.position.x <= targetPos.x)
             {
                 transform.position = targetPos;
+                pos = targetPos;
                 moveLeft = false;
+                Debug.Log(pos.x);
             }
         }
         if (moveRight)
         {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            Debug.Log("右");
+
+            if (pos.x < 0.0f)
+                pos.x *= -1.0f;
+            //if(pos.x >= 17.8f)
+            //{
+            //    pos.x -= moveSpeed / 8.0f;
+            //}
+            //else
+            //{
+            //    pos.x -= moveSpeed * 3.0f;
+            //}
+            //minusNum += 0.5f;
+            //pos.x -= moveSpeed / 3.3f;
+            if (pos.x >= 11.0f)
+            {
+                pos.x -= 0.17f;
+            }
+            else if (pos.x >= 1.0f)
+            {
+                pos.x -= 0.28f;
+            }
+            else
+            {
+                pos.x -= 1.0f;
+            }
+            //pos.x -= 0.186f;
+
+            Debug.Log(pos.x);
+
+            transform.Translate(Vector3.right * pos.x * Time.deltaTime);
             if (transform.position.x >= tmp.x)
             {
                 transform.position = tmp;
+                pos = new Vector3(0.0f, tmp.y, tmp.z); ;
+                minusNum = 0.0f;
                 moveRight = false;
+
+                // めくるモード → アクションモード
+                //CursorSystem.SetGameState(CursorSystem.GameState.MODE_ACTION);
             }
         }
     }

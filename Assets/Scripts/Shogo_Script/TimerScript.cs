93523fd39180c25cefa685306e4a6695fbbfd9e0
ShogoShimizu <sho_gogo_0104@icloud.com>
2022-05-23 14:18:29 +0900

    タイマーが出たり消えたり、出たり消えたり、出し入れしたり、課金したり、なんやかんやで終わりまshチア。

--- Assets/Scripts/Shogo_Script/TimerScript.cs
+++ Assets/Scripts/Shogo_Script/TimerScript.cs
@@ -13,6 +13,8 @@ public class TimerScript : MonoBehaviour
     private float oldSeconds;
     //　タイマー表示用テキスト
     private Text timerText;
+    // タイマー開始用フラグ
+    private bool bStart;
 
     //合計の時間
     private float SumTime;
@@ -23,36 +25,49 @@ public class TimerScript : MonoBehaviour
         seconds = 0f;
         oldSeconds = 0f;
         timerText = GetComponentInChildren<Text>();
+        bStart = false;
+        gameObject.GetComponent<Text>().enabled = bStart;
     }
 
     void Update()
     {
         GameObject player = GameObject.Find("ParentPlayer");
-
-        if (player.GetComponent<PlayerMove2>().GetFlg() && player.GetComponent<PlayerMove2>().GetGameOverFlg())
+        if (gameObject.GetComponent<Text>().enabled)
         {
-            seconds += Time.deltaTime;
-            if (seconds >= 60f)
+            if (player.GetComponent<PlayerMove2>().GetFlg() && player.GetComponent<PlayerMove2>().GetGameOverFlg())
             {
-                minute++;
-                seconds = seconds - 60;
-            }
-            //　値が変わった時だけテキストUIを更新
-            if ((int)seconds != (int)oldSeconds)
-            {
-                timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
-            }
-            oldSeconds = seconds;
+                seconds += Time.deltaTime;
+                if (seconds >= 60f)
+                {
+                    minute++;
+                    seconds = seconds - 60;
+                }
+                //　値が変わった時だけテキストUIを更新
+                if ((int)seconds != (int)oldSeconds)
+                {
+                    timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+                }
+                oldSeconds = seconds;
 
+            }
+            else
+            {
+                this.gameObject.SetActive(false);
+                bStart = false;
+            }
+            SumTime = seconds + (minute * 60);
         }
-        else
-        {
-            this.gameObject.SetActive(false);
-        }
-        SumTime = seconds + (minute * 60);
     }
 
-    public float GetTime() {
+    public float GetTime()
+    {
         return SumTime;
+    }
+
+    public void TimerStart()
+    {
+        // タイマー開始
+        bStart = true;
+        gameObject.GetComponent<Text>().enabled = bStart;
     }
 }
